6b3a0def8c1dc2cdd63c7d7bbc3b4bc19fb5d999
Overcluck <neuron09activation@gmail.com>
2026-06-17 15:11:18 +0900

    server fix

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,8 +9,8 @@ dotenv.config()
 const app = express()
 
 const allowedOrigins = [
-  'http://localhost:3000',
-  'http://127.0.0.1:3000'
+  'http://localhost:8080',
+  'http://127.0.0.1:8080'
 ]
 
 const corsOptions = {
